3f88bf74c03fe91bc7ce66bee3b74b627936af32
Luis Mendez <hallo@luis-mendez.de>
2026-05-30 11:48:31 +0200

    feat(settings): add registry barrel export

---
+++ src/features/settings/registry/index.ts
@@ -0,0 +1,12 @@
+export { buildDefaultsFromRegistry } from './buildDefaultsFromRegistry';
+export { USE_REGISTRY_RENDERER } from './featureFlag';
+export { readPath, writePath } from './path';
+export { getSettingsRegistry, resetSettingsRegistryForTests } from './registry';
+export type {
+  SettingsCtx,
+  SettingsField,
+  SettingsFieldType,
+  SettingsSection,
+  SettingsTab,
+} from './SettingsField';
+export { SettingsRegistry } from './SettingsRegistry';
